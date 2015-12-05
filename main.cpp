4f4c8d73fd3766a3a861a7a0638cb2d0f96ff660
Alexis Spahn <alexisspahn@gmail.com>
2015-12-04 21:45:17 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+//main
+//course: CS216-001
+//Project: Lab Assignment 12
+//Date: 11/28/2015
+//Purpose: to build a parse tree which shows the relationships
+//         among operators and operands in an expression
+//         display the tree structure visually
+//Author: Petia Nikolova
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "parsetree.h"
+
+using namespace std;
+
+int main()
+{
+	ParseTree exprTree; 
+	string infix;
+	bool more = true;
+					//Output to console screen instructions:
+	do
+	{
+		cout << endl;
+		cout << "Read an expression (type Q to quit):" << endl;
+		getline(cin,infix);
+
+		if(infix == "Q" || infix == "q")
+		{
+			more = false;
+		}
+		else
+		{ 
+                        exprTree.buildTree(infix);
+                        exprTree.printTree();
+		}
+	}while (more);
+} //end of program
---
+++ makefile
@@ -0,0 +1,2 @@
+Lab12: main.cpp 2parsetree.cpp 2parsetree.h
+	g++ -g -o Lab12 main.cpp 2parsetree.cpp
